fc721d8cfa7b8eb88befe3481fe8c008565f6d5c
Artsiom Tkachou <atkachou@ddn.com>
2016-06-24 17:56:00 +0300

    2

--- chess_horse.py
+++ chess_horse.py
@@ -1,20 +1,25 @@
 import collections
 
 
-def generate_horse_graph():
-    def get_acceptable_steps(i, j):
-        template = {(i + 1, j - 2), (i + 1, j + 2), (i - 1, j - 2),
-                    (i - 1, j + 2),
-                    (i + 2, j - 1), (i + 2, j + 1), (i - 2, j + 1),
-                    (i - 2, j - 1)}
-        result = []
-        for step in template:
-            if step[0] < 0 or step[0] > 7 or step[1] < 0 or step[1] > 7:
-                pass
-            else:
-                result.append(step)
-        return result
+def filter_steps():
+    pass
 
+
+def get_acceptable_steps(i, j):
+    template = {(i + 1, j - 2), (i + 1, j + 2), (i - 1, j - 2),
+                (i - 1, j + 2),
+                (i + 2, j - 1), (i + 2, j + 1), (i - 2, j + 1),
+                (i - 2, j - 1)}
+    result = []
+    for step in template:
+        if step[0] < 0 or step[0] > 7 or step[1] < 0 or step[1] > 7:
+            pass
+        else:
+            result.append(step)
+    return result
+
+
+def generate_horse_graph():
     graph = collections.defaultdict(list)
     for i in range(8):
         for j in range(8):
@@ -23,11 +28,27 @@ def generate_horse_graph():
     return dict(graph)
 
 
-def get_path(start_point, end_point, graph):
-    pass
+def path(graph, start, goal):
+    paths = collections.deque([[start]])
+    extended = set()
+    while len(paths) != 0:
+        current_path = paths.popleft()
+        current_point = current_path[-1]
+        if current_point == goal:
+            return current_path
+        elif current_point in extended:
+            continue
+        extended.add(current_point)
+        transforms = graph[current_point]
+        for word in transforms:
+            if word not in current_path:
+                paths.append(current_path[:] + [word])
+    return []
+
 
 graph = generate_horse_graph()
+# print graph
 sp = (0, 0)
 ep = (7, 7)
-print get_path(start_point=sp, end_point=ep, graph=graph)
 
+print path(graph, sp, ep)
